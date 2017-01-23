00c19fc8a1b764fb08427bdaca3493218ddd5639
Maximilian Schwarzmüller <mblacky0@gmail.com>
2017-01-23 15:42:45 +0100

    added signup

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function signup(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+
+        $user = new User([
+           'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+        $user->save();
+        return response()->json([
+            'message' => 'Successfully created user!'
+        ], 201);
+    }
+}
